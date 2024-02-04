4ee2c58d789de04a9b0ca6861637503cd150a2c4
ᴍᴀɴᴛɪs™◢ ◤ <157669020+Manti-Tm@users.noreply.github.com>
2024-02-04 12:52:53 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -52,9 +52,7 @@ async def save_group(bot, message):
                 except:
                     pass
             if WELCOM_PIC:
-                temp.MELCOW['welcome'] = await message.reply_photo(
-                                                 photo=(WELCOM_PIC),
-                                                 caption=(script.WELCOM_TEXT.format(u.mention, message.chat.title)),
+                temp.MELCOW['welcome'] = await message.reply_photo(photo=WELCOM_PIC, caption=WELCOM_TEXT.format(user=u.mention, chat=message.chat.title))
                                                  reply_markup=InlineKeyboardMarkup(
                                                                          [[
                                                                            InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url='https://t.me/cinema_flix_updates'),
