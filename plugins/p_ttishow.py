99b0a4772b3924cd3d2273a23312bf7d8be53034
ᴍᴀɴᴛɪs™◢ ◤ <157669020+Manti-Tm@users.noreply.github.com>
2024-02-05 12:24:25 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -52,13 +52,16 @@ async def save_group(bot, message):
                 except:
                     pass
             if WELCOM_PIC:
-                temp.MELCOW['welcome'] = await message.reply_photo(photo=WELCOM_PIC, caption=WELCOM_TEXT.format(user=u.mention, chat=message.chat.title))
+                temp.MELCOW['welcome'] = await message.reply_photo(
+                                                 photo=WELCOM_PIC, 
+                                                 caption=WELCOM_TEXT.format(user=u.mention, chat=message.chat.title))
                                                  reply_markup=InlineKeyboardMarkup(
-                                                                        [[
+                                                                         [[
                                                                            InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url='https://t.me/cinema_flix_updates'),
                                                                            InlineKeyboardButton('Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ', url='https://t.me/cinema_flix_updates')
-                                                                        ]]
+                                                                        ]]  
                                                  ),
+                                                 parse_mode=enums.ParseMode.HTML
               else:
                   temp.MELCOW['welcome'] = await message.reply_text(text=WELCOM_TEXT.format(user=u.mention, chat=message.chat.title))                               parse_mode=enums.ParseMode.HTML       
                                                    
