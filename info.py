c4b012be338a24412c423e265fed690586f86006
ᴍᴀɴᴛɪs™◢ ◤ <157669020+Manti-Tm@users.noreply.github.com>
2024-02-25 06:19:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -29,6 +29,7 @@ USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 PICS = (environ.get('PICS' ,'https://telegra.ph/file/79bd83439254ed9c4bbca.jpg')).split()
 NOR_IMG = environ.get('NOR_IMG', "https://graph.org/file/ebecf2e8bd05f866ea862.jpg")
 SPELL_IMG = environ.get('SPELL_IMG', "https://graph.org/file/0db1d70e5ef3213ed906b.jpg")
+MOVIE = environ.get('MOVIE', "https://telegra.ph/file/2151379f0abc3078ba112.jpg")
 BOT_START_TIME = time()
 
 # Admins, Channels & Users
