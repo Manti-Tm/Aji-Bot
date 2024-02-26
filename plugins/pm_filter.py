1b46ca575ad0dc124bd0bedf2797c678fc4fee59
ᴍᴀɴᴛɪs™◢ ◤ <157669020+Manti-Tm@users.noreply.github.com>
2024-02-26 09:50:06 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -296,17 +296,18 @@ async def advantage_spoll_choker(bot, query):
             await auto_filter(bot, query, k)
         else:
 
-            k = await query.message.edit(
-                      caption=(NOTFOUND),
-                      reply_markup=InlineKeyboardMarkup(
+            k = await query.message.reply_photo(
+                       photo=(MOVIE),
+                       caption=(NOTFOUND),
+                       reply_markup=InlineKeyboardMarkup(
                                               [[
                                                  InlineKeyboardButton('♻️ ᴍᴀʟʏᴀʟᴀᴍ  ♻️', 'trtoml'),
                                                  InlineKeyboardButton("ʙᴏᴛ oᴡɴᴇʀ", url="https://t.me/Master_brooi")
                                              ],[
                                                  InlineKeyboardButton(" ʜᴏᴡ ᴛᴏ ᴍᴇɴᴛɪᴏɴ​ ᴀᴅᴍɪɴ​", url="https://graph.org/How-To-Get-Movies-From-Bot-12-03")
                                              ]] 
-                     ),
-                     parse_mode='html'
+                      ),
+                      parse_mode='html'
               )
             
             await asyncio.sleep(10)
@@ -329,20 +330,23 @@ async def pm_spoll_tester(bot, query):
         await pm_AutoFilter(bot, query, k)
     else:
         
-        k = await query.message.edit(
-                      caption=(NOTFOUND),
-                      reply_markup=InlineKeyboardMarkup(
-                                              [[
-                                                 InlineKeyboardButton('♻️ ᴍᴀʟʏᴀʟᴀᴍ  ♻️', 'trtoml'),
-                                                 InlineKeyboardButton("ʙᴏᴛ oᴡɴᴇʀ", url="https://t.me/Master_brooi")
-                                             ],[
-                                                 InlineKeyboardButton(" ʜᴏᴡ ᴛᴏ ᴍᴇɴᴛɪᴏɴ​ ᴀᴅᴍɪɴ​", url="https://graph.org/How-To-Get-Movies-From-Bot-12-03")
-                                             ]] 
-                     ),
-                     parse_mode='html'
-              )
-        await asyncio.sleep(10)
-        await k.delete()
+        k = await query.message.reply_photo(
+                   photo=(MOVIE),
+                   caption=(NOTFOUND),
+                   reply_markup=InlineKeyboardMarkup(
+                                          [[
+                                             InlineKeyboardButton('♻️ ᴍᴀʟʏᴀʟᴀᴍ  ♻️', 'trtoml'),
+                                             InlineKeyboardButton("ʙᴏᴛ oᴡɴᴇʀ", url="https://t.me/Master_brooi")
+                                          ],[
+                                              InlineKeyboardButton(" ʜᴏᴡ ᴛᴏ ᴍᴇɴᴛɪᴏɴ​ ᴀᴅᴍɪɴ​", url="https://graph.org/How-To-Get-Movies-From-Bot-12-03")
+                                          ]] 
+                   
+                  ),
+                  parse_mode='html'
+        )
+        
+      await asyncio.sleep(10)
+      await k.delete()
 
 
 @Client.on_callback_query()
